6cd223b064c073d3636ca139fdce2e35ae1e453d
Andrew Hill <andrew.hill.w@gmail.com>
2021-05-19 22:06:14 -0400

    Playlist and Youtube Player

--- motivateme/src/App.tsx
+++ motivateme/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import YoutubeEmbed from './YoutubeEmbed.js';
 import './App.css';
 
-function App() {
+function Videos() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to update.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS" frameBorder="0" allowFullScreen></iframe>
       </header>
     </div>
   );
 }
 
-export default App;
+
+export default Videos;
