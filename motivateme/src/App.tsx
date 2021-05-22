09a3cd5b5dc5b54cbb22e81622b827e4543caa5f
Andrew Hill <andrew.hill.w@gmail.com>
2021-05-22 15:53:20 -0400

    Restore Point before nav bar

--- motivateme/src/App.tsx
+++ motivateme/src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import YoutubeEmbed from './YoutubeEmbed.js';
+import { FaPhotoVideo } from 'react-icons/fa'
+import { NavigationContainer } from 'react-navigation';
 import './App.css';
 
 function Videos() {
   return (
     <div className="App">
-      <header className="App-header">
-      <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS" frameBorder="0" allowFullScreen></iframe>
-      </header>
+      <div className="vidContainer">
+        <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS" frameBorder="0" allowFullScreen></iframe>
+
+      </div>
+
+      <div className="navBar">
+        <div><FaPhotoVideo color="blue" size="2em"/></div>
+        <div><FaPhotoVideo color="blue" size="2em"/></div>
+        <div><FaPhotoVideo color="blue" size="2em"/></div>
+        <div><FaPhotoVideo color="blue" size="2em"/></div>
+      </div>
+
+
+      
     </div>
   );
 }
