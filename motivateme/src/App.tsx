36bdf449fb7768de0e3b0c35dc8551c3221bda15
Andrew Hill <andrew.hill.w@gmail.com>
2021-05-25 11:18:30 -0400

    Generating Random Video Functionality

--- motivateme/src/App.tsx
+++ motivateme/src/App.tsx
@@ -5,34 +5,33 @@ import { FaPhotoVideo, FaRegLightbulb } from 'react-icons/fa';
 import './App.css';
 
 
-var random: number;
+let vidCount: number;
 
-async function getNewUrl(){
-  debugger;
+async function getVidCount(){
   const response = await fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS&key=AIzaSyDshUVKt-LXwfNPt6gkchEmFs-4AmYSDB0');
   const body = await response.json();
-  const count = body.items.length;
-  random = Math.floor(Math.random()*count);
+  vidCount = body.items.length;
 }
 
 
-function reloadPage(){
-  window.location.reload();
+function swapVideos(){
+  let random = Math.floor(Math.random()*vidCount);
+  console.log(random);
+  let videoEmbed = document.getElementById('vid');
+  let vidUrl = "https://www.youtube.com/embed?listType=playlist&list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS&index="+random;
+  console.log(vidUrl);
+  videoEmbed?.setAttribute('src',vidUrl);
 }
 
 function Videos() {
-  getNewUrl();
-  console.log(random);
-  let vidUrl = "https://www.youtube.com/embed/videoseries?list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS&index="+random;
-
-
+  getVidCount();
   return (
     <div className="App">
       <div className="vidContainer">
-        <iframe id="vid" width="560" height="315" src={vidUrl} frameBorder="0" allowFullScreen></iframe>
+        <iframe id="vid" width="560" height="315" src="https://www.youtube.com/embed?listType=playlist&list=PLStIAJ2xkB0sfTRdXNQ-aIeqWxzSOgBtS&index=0" frameBorder="0" allowFullScreen></iframe>
       </div>
 
-      <div><button onClick={reloadPage}><FaRegLightbulb color="#61dafb" size="2em"/></button></div>
+      <div><button onClick={swapVideos}><FaRegLightbulb color="#61dafb" size="2em"/></button></div>
 
       <div className="navBar">
         <div><FaPhotoVideo color="#61dafb" size="2em"/></div>
